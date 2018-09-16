c1715ad37d166e403099e32b592506a68b109074
Artūrs <akempelis@gmail.com>
2018-09-16 16:47:33 +0300

    Adding service documentation, and database interaction methods

---
+++ src/main/java/persistence/UserPs.java
@@ -0,0 +1,110 @@
+package persistence;
+
+import entity.User;
+import utility.DBUtil;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class UserPs {
+
+    public UserPs() {
+    }
+
+    public static void createNewUser(User user, String passkey) throws SQLException {
+        try{
+            PreparedStatement preparedStatement = DBUtil.getConnection().prepareStatement("insert into user (username, passphrase, created, status) values (?, ?, ?, ?)");
+            preparedStatement.setString(1, user.getUsername());
+            preparedStatement.setString(2, passkey);
+            preparedStatement.setString(3, user.getTimeCreated());
+            preparedStatement.setString(4, user.getStatus());
+            preparedStatement.executeUpdate();
+        }catch (Exception ex){
+            System.out.println("Error saving new user" + ex);
+        } finally {
+            DBUtil.getConnection().close();
+        }
+    }
+
+    public static List<User> getUsers() throws SQLException {
+        List<User> users = new ArrayList<>();
+        try {
+            Statement statement = DBUtil.getConnection().createStatement();
+            ResultSet rs = statement.executeQuery("select * from user where username=0");
+            while (rs.next()) {
+                User user = new User();
+                user.setId(rs.getInt("id"));
+                user.setUsername(rs.getString("username"));
+                user.setTimeCreated(rs.getString("created"));
+                user.setStatus(rs.getString("status"));
+                users.add(user);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            DBUtil.getConnection().close();
+        }
+        return users;
+    }
+
+    public static void updateUser(User user, String status) throws SQLException {
+        try{
+            PreparedStatement preparedStatement = DBUtil.getConnection().prepareStatement("update user set status = ? where username = ?");
+            preparedStatement.setString(1, status);
+            preparedStatement.setString(2, user.getUsername());
+            preparedStatement.executeUpdate();
+        }catch (Exception ex){
+            System.out.println("Error updating user" + ex);
+        } finally {
+            DBUtil.getConnection().close();
+        }
+    }
+
+    public static User getUser(String username) throws SQLException {
+        User user = new User();
+        try{
+            PreparedStatement preparedStatement = DBUtil.getConnection().prepareStatement("select * from user where username = ?");
+            preparedStatement.setString(1, username);
+            ResultSet rs = preparedStatement.executeQuery();
+
+            while (rs.next()) {
+                user.setId(rs.getInt("id"));
+                user.setTimeCreated(rs.getString("created"));
+                user.setUsername(rs.getString("username"));
+                user.setStatus(rs.getString("status"));
+            }
+            return user;
+        }catch (SQLException ex){
+            System.out.println("Error fetching user " + ex);
+            return null;
+        } finally {
+            DBUtil.getConnection().close();
+        }
+    }
+
+    public static void deleteUser(String username) throws SQLException {
+        try{
+            PreparedStatement preparedStatement = DBUtil.getConnection().prepareStatement("delete from user where username = ?");
+            preparedStatement.setString(1, username);
+            preparedStatement.executeUpdate();
+        }catch (Exception ex){
+            System.out.println("Error deleting user  " + ex);
+        } finally {
+            DBUtil.getConnection().close();
+        }
+    }
+
+    public static String getUserPasskey(String username) throws SQLException {
+        String storedPasskey = null;
+        PreparedStatement preparedStatement = DBUtil.getConnection().prepareStatement("select * from user where username = ?");
+        preparedStatement.setString(1, username);
+        ResultSet rs = preparedStatement.executeQuery();
+        while (rs.next()) {
+            storedPasskey = rs.getString("passphrase");
+        }
+        return storedPasskey;
+    }
+
+}
---
+++ src/main/java/service/UserManagerService.java
@@ -0,0 +1,82 @@
+package service;
+
+import entity.User;
+import persistence.UserPs;
+
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserManagerService {
+
+    public UserManagerService() {
+
+    }
+
+    public List<User> getAllUsers() throws SQLException{
+        return UserPs.getUsers();
+    }
+
+    public List<User> getUser(String username) throws SQLException, BusinessException {
+        List<User> users = new ArrayList<>();
+        User user = UserPs.getUser(username);
+        if(user != null){
+            users.add(user);
+        }else{
+            throw BusinessException.create(EGeneralErrorCode.BAD_REQUEST, EUserErrorDetail.USERS_USERNAME_NOT_FOUND.getMessage());
+        }
+        return users;
+    }
+
+    public User addUser(String username, String passkey) throws SQLException, BusinessException {
+        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+        User userToCreate = new User();
+
+        userToCreate.setUsername(username);
+        userToCreate.setTimeCreated(timestamp.toString());
+        userToCreate.setStatus(UserStatus.CREATED.getMessage());
+
+        String hashedPasskey = null;
+        try {
+            hashedPasskey = PassUtil.getSaltedHash(passkey);
+        } catch (Exception e) {
+            throw BusinessException.create(EGeneralErrorCode.APPLICATION_ERROR, EUserErrorDetail.USER_PASSWORD_COULD_NOT_BE_HASHED.getMessage());
+        }
+
+        UserPs.createNewUser(userToCreate, hashedPasskey);
+        return userToCreate;
+    }
+
+    public User updateUser(String username, String passkey, String status) throws Exception {
+        User user = UserPs.getUser(username);
+        if(user == null){
+            throw BusinessException.create(EGeneralErrorCode.BAD_REQUEST, EUserErrorDetail.USERS_USERNAME_NOT_FOUND.getMessage());
+        }
+        if(checkPasskey(username, passkey)){
+            UserPs.updateUser(user, status);
+        }else{
+            throw BusinessException.create(EGeneralErrorCode.APPLICATION_ERROR, EUserErrorDetail.USER_PASSWORD_INCORRECT.getMessage());
+        }
+        return null;
+    }
+
+    public void deleteUser(String username) throws SQLException, BusinessException {
+        if(username == null){
+            throw BusinessException.create(EGeneralErrorCode.BAD_REQUEST, EUserErrorDetail.USERS_USERNAME_INVALID.getMessage());
+        }
+        UserPs.deleteUser(username);
+    }
+
+    public boolean usernameExists(String username) throws SQLException {
+        List<User> users = new ArrayList<>(UserPs.getUsers());
+        for(User user : users){
+            if(user.getUsername().equals(username)){
+                //username already exists
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
